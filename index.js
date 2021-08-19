8d432fedc229aadfddee9581ffdd1c2688234891
VaishakAnand <vaishak140498@gmail.com>
2021-08-19 12:25:26 +0800

    Add input validation for priceAlert

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ bot.command("start", (ctx) => {
 bot.command("priceAlert", (ctx) => {
   const args = ctx.update.message.text.split(" ");
 
-  if (args != 4) {
+  if (args.length != 4) {
     bot.telegram.sendMessage(
       ctx.chat.id,
       "Please input the correct number of arguments!\nE.g: /priceAlert ETH > 3000",
@@ -27,8 +27,23 @@ bot.command("priceAlert", (ctx) => {
   const comparison = args[2];
   const value = args[3];
 
-  if (!isNaN(args[3])) {
-    
+  const acceptableComparisons = [">", "<", "=", "<=", ">="];
+  if (!isNaN(value)) {
+    bot.telegram.sendMessage(
+      ctx.chat.id,
+      "The value has to be a number!\nE.g: /priceAlert ETH > 3000",
+      {}
+    );
+  } else if (!acceptableComparisons.includes(comparison)) {
+    bot.telegram.sendMessage(
+      ctx.chat.id,
+      "Please input an appropriate comparison symbol!\n" +
+        "Choose one from this list: [ >, <, =, >=, <= ]" +
+        "\nE.g: /priceAlert ETH > 3000",
+      {}
+    );
   }
+
+  
 });
 bot.launch();
