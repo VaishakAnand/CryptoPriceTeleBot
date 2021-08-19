30aea8c72c4d7763b48335d060b92cb35b7cec6b
VaishakAnand <vaishak140498@gmail.com>
2021-08-19 17:00:25 +0800

    Add basic functionality for price checking and returning

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const Telegraf = require("telegraf");
+const axios = require("axios");
+const { Spot } = require("@binance/connector");
 
 const bot = new Telegraf.Telegraf(process.env.BOT_TOKEN);
+const client = new Spot();
 
 bot.command("start", (ctx) => {
   console.log(ctx.from);
@@ -18,9 +21,10 @@ bot.command("priceAlert", (ctx) => {
   if (args.length != 4) {
     bot.telegram.sendMessage(
       ctx.chat.id,
-      "Please input the correct number of arguments!\nE.g: /priceAlert ETH > 3000",
+      "Please input the correct number of arguments!\nE.g: /priceAlert ETHUSDT > 3000",
       {}
     );
+    return;
   }
 
   const ticker = args[1];
@@ -28,22 +32,82 @@ bot.command("priceAlert", (ctx) => {
   const value = args[3];
 
   const acceptableComparisons = [">", "<", "=", "<=", ">="];
-  if (!isNaN(value)) {
+  if (isNaN(value)) {
     bot.telegram.sendMessage(
       ctx.chat.id,
-      "The value has to be a number!\nE.g: /priceAlert ETH > 3000",
+      "The value has to be a number!\nE.g: /priceAlert ETHUSDT > 3000",
       {}
     );
+    return;
   } else if (!acceptableComparisons.includes(comparison)) {
     bot.telegram.sendMessage(
       ctx.chat.id,
       "Please input an appropriate comparison symbol!\n" +
         "Choose one from this list: [ >, <, =, >=, <= ]" +
-        "\nE.g: /priceAlert ETH > 3000",
+        "\nE.g: /priceAlert ETHUSDT > 3000",
       {}
     );
+    return;
   }
 
-  
+  function pingTillSuccess(messageId) {
+    setTimeout(() => {
+      return client.tickerPrice(ticker).then((response) => {
+        const price = response.data.price;
+        console.log(response.data);
+
+        let isSuccess = true;
+        switch (comparison) {
+          case ">":
+            isSuccess = price > value;
+            break;
+          case "<":
+            isSuccess = price < value;
+            break;
+          case "=":
+            isSuccess = price = value;
+            break;
+          case ">=":
+            isSuccess = price >= value;
+            break;
+          case "<=":
+            isSuccess = price <= value;
+            break;
+          default:
+            console.log(
+              "ERROR: Reached impossible default of switch case for printAlert"
+            );
+        }
+
+        if (!isSuccess) {
+          bot.telegram.editMessageText(
+            ctx.chat.id,
+            messageId,
+            undefined,
+            `Will notify you when ${ticker} ${comparison} ${value}\nLatest pinged price: ${price}`
+          );
+          pingTillSuccess(messageId);
+        } else {
+          bot.telegram.sendMessage(
+            ctx.chat.id,
+            `Reached target alert price value for ${ticker}\nTarget: ${comparison} ${value}\nCurrent: ${price}`,
+            {}
+          );
+        }
+      });
+    }, 10000);
+  }
+
+  return bot.telegram
+    .sendMessage(
+      ctx.chat.id,
+      `Will notify you when ${ticker} ${comparison} ${value}`,
+      {}
+    )
+    .then((messageDetails) => {
+      const messageId = messageDetails.message_id;
+      pingTillSuccess(messageId);
+    });
 });
+
 bot.launch();
