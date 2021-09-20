ddf605d0d70f1c59a0fb014765518edf833000f6
Vaishak <vaishak140498@gmail.com>
2021-09-20 16:47:01 +0800

    Set up router

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/alert", (req, res) => {
-
+    console.log(req.body);
+    res.status(200).send("ok");
 }); 
 
 module.exports = router;
