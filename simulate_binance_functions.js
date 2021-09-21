66e708fe89419dc62a6690be657611c6f51b363e
Vaishak <vaishak140498@gmail.com>
2021-09-21 23:31:03 +0800

    Add binance api and cover most of simulate purchase

---
+++ simulate_binance_functions.js
@@ -0,0 +1,117 @@
+require("dotenv").config();
+const Binance = require("node-binance-api");
+const binance = new Binance().options({
+  APIKEY: process.env.BINANCE_API_KEY,
+  APISECRET: process.env.BINANCE_API_SECRET,
+});
+
+function buy_with_leverage(symbol, priceToBuy) {
+  const side = "BUY";
+  // change margin type for symbol
+  // POST /fapi/v1/marginType (HMAC SHA256)
+  // symbol, marginType = ISOLATED, timestamp
+
+  // change leverage for symbol
+  // POST /fapi/v1/leverage (HMC)
+  // symbol, leverage (1 to 125), timestamp (current time stamp in ms)
+
+  // post buy order
+  // POST /fapi/v1/order (HMAC SHA256)
+  // symbol, side = BUY, type = LIMIT,
+
+  // Query buy order till filled
+  // post sell at stop loss order
+  // post sell at take profit order
+}
+
+function rngAmount(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function simulatePurchase(symbol) {
+  binance.futuresMarkPrice(symbol).then((price) => {
+    // Place order:
+    //  - Market Order -> Add rng amount
+    //  - Limit Order -> Use estimatedSellingPrice
+    const estimatedSellingPrice = price.estimatedSellingPrice;
+    const marketOrderPrice = rngAmount(0.99, 1.01) * estimatedSellingPrice;
+    const limitOrderPrice = estimatedSellingPrice;
+
+    // Set Leverage
+    const leverage = 25;
+    // Set Margin Type
+    const marginType = "ISOLATED";
+    const marginAmount = 100;
+
+    // Make purchase
+    setTimeout(() => {
+      // Purchase waiting to be filled
+    }, rngAmount(0, 10000));
+
+    // Number of units bought
+    const marketOrderUnits = (leverage * marginAmount) / marketOrderPrice;
+    const limitOrderUnits = (leverage * marginAmount) / limitOrderPrice;
+    // Fees
+    const marketOrderFees = leverage * marginAmount * (0.036 / 100);
+    const limitOrderFees = leverage * marginAmount * (0.018 / 100);
+
+    // Stop losses and take profit prices
+    const marketOrderStopLossPrice = 0.95 * marketOrderPrice;
+    const marketOrderTakeProfitPrice = 1.1 * marketOrderPrice;
+    const limitOrderStopLossPrice = 0.95 * limitOrderPrice;
+    const limitOrderTakeProfitPrice = 1.1 * limitOrderPrice;
+
+    // Wait for either stop loss or take profit to hit
+    let latestPrice = 0;
+    let marketOrderRunning = true;
+    let limitOrderRunning = true;
+    let marketOrderSalePrice = -1;
+    let limitOrderSalePrice = -1;
+    const websocket = binance.futuresMarkPriceStream(symbol, (prices) => {
+      const indexPrice = prices.indexPrice;
+      latestPrice = indexPrice;
+    });
+    const interval = setInterval(function () {
+      // Maybe add the red alert signal here
+      if (
+        marketOrderRunning &&
+        (latestPrice <= marketOrderStopLossPrice ||
+          latestPrice >= marketOrderTakeProfitPrice)
+      ) {
+        marketOrderSalePrice = latestPrice;
+      }
+
+      if (
+        limitOrderRunning &&
+        (latestPrice <= limitOrderStopLossPrice ||
+          latestPrice >= limitOrderTakeProfitPrice)
+      ) {
+        limitOrderSalePrice = latestPrice;
+      }
+
+      if (!marketOrderRunning && !limitOrderRunning) {
+        binance.futuresTerminate(websocket);
+        clearInterval(interval);
+      }
+    }, 1000);
+
+  });
+}
+
+function run() {
+  let latestPrice = 0;
+  const ws = binance.futuresMarkPriceStream("SOLUSDT", (prices) => {
+    const indexPrice = prices.indexPrice;
+    console.log(indexPrice);
+    latestPrice = indexPrice;
+  });
+  // console.log("WS = ", ws);
+  setInterval(function () {
+    // Terminate all websocket endpoints, every 6 sec
+    if (latestPrice > 120) {
+      binance.futuresTerminate(ws);
+      process.exit(0);
+    }
+  }, 1000);
+}
+run();
